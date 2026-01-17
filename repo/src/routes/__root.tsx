fcc55db664d3cca3b831116c29334bac4b629fe9
Eduardo Rodrigues <eduardoanjosrodrigues@gmail.com>
2026-01-17 02:04:58 -0300

    sem devtools

--- repo/src/routes/__root.tsx
+++ repo/src/routes/__root.tsx
@@ -1,9 +1,4 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
-
 import type { QueryClient } from '@tanstack/react-query'
 
 interface MyRouterContext {
@@ -14,20 +9,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <div className="min-h-screen bg-rose-50 text-slate-900">
       <Outlet />
-      {import.meta.env.DEV ? (
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            TanStackQueryDevtools,
-          ]}
-        />
-      ) : null}
     </div>
   ),
 })
